36586114b8a354b0f5811780a0bc73c0f712d5bc
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2021-10-31 23:01:23 +0800

    Create Importdata.py

---
+++ Importdata.py
@@ -0,0 +1,39 @@
+import json
+import pandas as pd
+import pymongo
+
+# get each collection name
+def get_sheetname(inputfile):
+    jsonfile = pd.read_json(inputfile, orient="index")
+    sheetname = jsonfile.index
+
+    return sheetname
+
+
+# Replace the uri string with your MongoDB deployment's connection string.
+Conn_str = "mongodb://localhost:27017/"
+Filepath = "data.json"
+
+# set a 5-second connection timeout
+client = pymongo.MongoClient(Conn_str, serverSelectionTimeoutMS=5000)
+
+try:
+   print(client.list_database_names())
+except Exception:
+    print("Unable to connect to the server.")
+
+db = client["example2db"]
+
+col_name = get_sheetname(Filepath)
+
+print(col_name[0])
+
+with open(Filepath) as file:
+    file_data = json.load(file)
+
+for i in col_name: 
+    new_col = db[i]
+    x = new_col.insert_many(file_data[i])
+    new_col = None
+
+#print(collection.inserted_ids)
